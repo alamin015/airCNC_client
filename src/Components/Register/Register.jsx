0bd7ad82e46c19a0e25ec0101aa2f19f3b7c1ef6
Al Amin <alamin>
2023-06-04 16:56:44 +0600

    margin padding fix in register

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -5,14 +5,13 @@ import Spinner from '../Spinner/Spinner';
 const Register = () => {
     const [loader,setLoader] = useState(true);
 
-    const handleLoader = (e) => {
-        e.preventDefault();
-        setLoader(!loader)
-    }
+    // const handleLoader = (e) => {
+    //     e.preventDefault();
+    //     setLoader(!loader)
+    // }
   return (
     <div className='container mx-auto h-screen flex items-center justify-center'>
         <form className='w-full max-w-[400px] mx-auto'>
-            <button onClick={handleLoader}>click</button>
             <div className='mb-3'>
                 <label htmlFor="name" className='block text-xl mb-1 font-medium'>Name:</label>
                 <input type="text" id='name' name="name" className='border rounded-md p-2 outline-none w-full'/>
@@ -29,7 +28,7 @@ const Register = () => {
                 <label htmlFor="image" className='block text-xl mb-1 font-medium border-2 border-dashed py-3 text-center cursor-pointer'>Upload Your Photo</label>
                 <input type="file" id='image' name="image" className='border rounded-md p-2 outline-none w-full hidden'/>
             </div>
-            <div className='text-center'>
+            <div className='text-center mt-10'>
                 <button className='h-[45px] w-[170px] bg-rose-500 text-xl font-medium text-white rounded-sm'>{loader ? <Spinner /> : 'Register' }</button>
                 
             </div>
